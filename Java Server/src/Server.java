ccd39afe35eea1c7ab9712bb27f403bef5ab0e46
jp16756 <44197949+jp16756@users.noreply.github.com>
2019-11-04 00:13:30 +0000

    Creating Matchmaking thread that will hold players whilst we wait for a pair

--- Java Server/src/Server.java
+++ Java Server/src/Server.java
@@ -8,28 +8,44 @@ import java.util.Date;
 import java.util.TreeMap;
 
 public class Server {
-    private static String serverString = "[SERVER] ";
-    private static int threadCount;
     public static void main(String[] args) {
+        MatchMaker matchMaker = new MatchMaker();
+        matchMaker.start();
+    }
+}
+
+
+class MatchMaker extends Thread {
+    private static String serverString = "[SERVER] ";
+    ServerSocket serverSocket;
+    // Constructor
+    public MatchMaker() {
         try {
-            ServerSocket serverSocket = new ServerSocket(5560);
+            serverSocket = new ServerSocket(5560);
             System.out.println(serverString + "Started");
             System.out.println(serverString + "Awaiting first client connection");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    @Override
+    public void run() {
+        try {
             while(true){
                 Socket s = null;
                 try {
                     s = serverSocket.accept();
 
-                    System.out.println(serverString + "New Client Connection" + s);
+                    //Got A player
+                    //Add them to to waiting queue (List of Sockets?)
+                    //Theres a pair?
+                    //Create a game server
+                    //With 2 Sockets;
+                    //Game Server Class
+                    //Holds Player 1 and 2 Socket
 
-                    DataInputStream dataInputStream = new DataInputStream(s.getInputStream());
-                    DataOutputStream dataOutputStream = new DataOutputStream(s.getOutputStream());
-                    Thread t = new ClientHandler(s, dataInputStream, dataOutputStream);
-                    t.start();
-                    threadCount++;
-                    System.out.println(serverString + "Creating new thread for client connection: THREADID:" + t.getId());
-                    System.out.println(serverString + "Player count : " + threadCount);
+
 
 
                 }catch (Exception e){
@@ -38,62 +54,6 @@ public class Server {
                 }
             }
         } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
-
-class ClientHandler extends Thread
-{
-    DateFormat fordate = new SimpleDateFormat("yyyy/MM/dd");
-    DateFormat fortime = new SimpleDateFormat("hh:mm:ss");
-    final DataInputStream dis;
-    final DataOutputStream dos;
-    final Socket s;
-
-
-    // Constructor
-    public ClientHandler(Socket s, DataInputStream dis, DataOutputStream dos)
-    {
-        this.s = s;
-        this.dis = dis;
-        this.dos = dos;
-    }
-
-    @Override
-    public void run()
-    {
-        String received;
-        String toreturn;
-        while (true)
-        {
-            try {
-                dos.writeUTF("What do you want?[Date | Time]..\n"+
-                        "Type Exit to terminate connection.");
-
-                received = dis.readUTF();
-
-                if(received.equals("Exit"))
-                {
-                    System.out.println("Client " + this.s + " sends exit...");
-                    System.out.println("Closing this connection.");
-                    this.s.close();
-                    System.out.println("Connection closed");
-                    break;
-                }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        try
-        {
-            // closing resources
-            this.dis.close();
-            this.dos.close();
-
-        }catch(IOException e){
             e.printStackTrace();
         }
     }
